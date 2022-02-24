db331e68fc28743d15f54ee78aab9150ec352726
santhosh <santhosh@skcript.com>
2022-02-24 07:37:22 +0530

    added devise for user authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   include ApiUtils
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+
+  def set_current_user
+    if current_user.present?
+      @current_user = current_user
+    end
+  end
 end
